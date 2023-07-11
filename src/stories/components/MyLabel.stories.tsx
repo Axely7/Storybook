d9f7c643cd0d49578042cb1b865167f50a3df670
Axely7 <axely7@hotmail.com>
2023-07-11 10:08:26 -0600

    colors, changing argTypes

--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -4,6 +4,11 @@ import { MyLabel } from "../../components/MyLabel";
 export default {
   title: "UI/MyLabel",
   component: MyLabel,
+  argTypes: {
+    size: { control: "select" },
+    color: { control: "select" },
+  },
+  tags: ["autodocs"],
 } as Meta<typeof MyLabel>;
 
 const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args} />;
@@ -11,8 +16,26 @@ const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args} />;
 export const Basic = Template.bind({});
 Basic.args = {
   size: "normal",
+  AllCaps: false,
 };
 
 export const AllCaps = Template.bind({});
 
+AllCaps.args = {
+  size: "normal",
+  AllCaps: true,
+};
+
 export const Secondary = Template.bind({});
+
+Secondary.args = {
+  size: "normal",
+  color: "secondary",
+};
+
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  size: "normal",
+  color: "tertiary",
+};
